66c5615a4f5cd13e67afeadae3dece8e233d7644
rtldg <55846624+rtldg@users.noreply.github.com>
2023-11-22 16:31:21 +0000

    Hello, World!

---
+++ PerksOfYore.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Reflection;
+
+using MelonLoader;
+using UnityEngine;
+
+// https://melonwiki.xyz/#/modders/quickstart
+
+/*
+desired mod features:
+- noclip
+- checkpoint / savestate (likely hard)
+*/
+
+namespace perksofyore
+{
+    public class PerksOfYore : MelonMod
+    {
+        GUIStyle heightStyle = new GUIStyle();
+        public override void OnInitializeMelon()
+        {
+            heightStyle.fontSize = 35;
+            heightStyle.normal.textColor = new Color(255.0f/255.0f, 95.0f/255.0f, 31.0f/255.0f);
+            MelonEvents.OnGUI.Subscribe(DrawHeight);
+        }
+        public override void OnUpdate()
+        {
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            //LoggerInstance.Msg($"Scene {sceneName} with build index {buildIndex} has been loaded!");
+            SetupHeight();
+        }
+
+        private float currentMetresUp;
+        private float summitHeight;
+        private GameObject playerGameobject;
+        private Transform playerTransformStartPosition;
+        private Transform playerTransform;
+        private void SetupHeight()
+        {
+            //if (!isMenu && !isDeathScene && !isEnding && !isCabin && !isCabin4)
+            {
+                var po = GameObject.FindGameObjectWithTag("Player");
+                var so = GameObject.FindGameObjectWithTag("SummitBox");
+                if (!po || !so)
+                {
+                    currentMetresUp = 0;
+                    summitHeight = 0;
+                    return;
+                }
+                playerTransform = po.GetComponent<Transform>();
+                var peakSummit = so.GetComponent<StamperPeakSummit>();
+                if (!playerTransform || !peakSummit)
+                {
+                    currentMetresUp = 0;
+                    summitHeight = 0;
+                    return;
+                }
+                playerGameobject = new GameObject("PlayerStartPosition");
+                playerGameobject.transform.position = playerTransform.position;
+                playerTransformStartPosition = playerGameobject.transform;
+                playerTransformStartPosition.position = playerTransform.position;
+                var ground = new Vector3(peakSummit.transform.position.x, playerTransformStartPosition.position.y, peakSummit.transform.position.z);
+                summitHeight = Vector3.Distance(ground, peakSummit.transform.position) + 0.75f /*playerheight*/;
+            }
+        }
+        private void CalculateHeights()
+        {
+            if (!playerTransform || !playerTransformStartPosition)
+            {
+                currentMetresUp = 0;
+                summitHeight = 0;
+                return;
+            }
+
+            currentMetresUp = Vector3.Distance(new Vector3(playerTransform.position.x, playerTransformStartPosition.position.y, playerTransform.position.z), playerTransform.position);
+        }
+        private void DrawHeight()
+        {
+            CalculateHeights();
+            if (currentMetresUp > 0 && summitHeight > 0)
+            {
+                //string s = String.Format("Height: {0:F2}m (%{1:F0}) {2:F2}", currentMetresUp, currentMetresUp / summitHeight * 100.0f, summitHeight);
+                string s = String.Format("Height: {0:F1}m (%{1:F0})", currentMetresUp, currentMetresUp / summitHeight * 100.0f);
+                GUI.Box(new Rect(6, 300, 100, 100), s, heightStyle);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using MelonLoader;
+using perksofyore;
+
+[assembly: MelonInfo(typeof(PerksOfYore), "Perks of Yore", "1.0.0", "rtldg")]
+//[assembly: MelonGame("Anders Grube Jensen", "Peaks of Yore")]
+[assembly: MelonGame("", "")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("perksofyore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("perksofyore")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f7d83be-3bed-44e1-a6a0-b78775c67192")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
